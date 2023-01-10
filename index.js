f120a3a58d166ba3468c501415888451f2cab806
Paulo Dias <paulovitorsouza99@gmail.com>
2023-01-09 21:25:19 -0300

    Validando campos do formulário no servidor

--- index.js
+++ index.js
@@ -25,12 +25,49 @@ app.use(session(({
 app.use(flash())
 
 app.get('/', (req, res)=>{
-    console.log(' está rodando')
-    res.render('index')
+    
+    var email = req.flash('email')
+    var nome = req.flash('nome')
+    var pontos = req.flash('pontos')
+
+    res.render('index', {
+        nome: nome,
+        email: email,
+        pontos: pontos
+    })
 })
 
 app.post('/form', (req, res)=>{
     var {email, nome, pontos} = req.body
+    
+
+    if(email == undefined || email ==""){
+        var emailError =  " campo email não pode ser vazio"
+    }
+    if(nome == undefined || nome == ""){
+        var nomeError = "campo nome não pode ser vazio"
+    }else if(nome.length < 4){
+        nomeError = " campo deve ter no mínimo 4 caracteres"
+    }
+    if(pontos == undefined || pontos == ""){
+        if(pontos <= 10){
+             var pontosError = " campo pontos deve ser maior que 10"
+        }
+    }
+
+    
+
+    if(emailError!= undefined || nomeError != undefined || pontosError != undefined){
+        
+        req.flash('email', email)
+        req.flash('nome', nome)
+        req.flash('pontos', pontos)
+       res.redirect('/')
+    }else{
+        res.send('Dados enviados')
+    }
+
+    
 
 
     
